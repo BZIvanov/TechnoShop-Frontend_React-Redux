112d3867e6b41bf83b72f192e95f1dbada136fca
BZIvanov <ivanov.biser@gmx.com>
2023-08-29 23:57:34 +0300

    custom throttler guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,22 @@
+import { APP_GUARD } from '@nestjs/core';
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { CustomThrottlerGuard } from './guards/custom-throttler.guard';
+import { MoviesModule } from './modules/movies/movies.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 100,
+    }),
+    MoviesModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: CustomThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
