842a32404d0cc6a1718b45f6d1b7d01447468134
BZIvanov <ivanov.biser@gmx.com>
2024-01-02 17:56:03 +0200

    store backend url in env variable

--- src/providers/store/services/api.js
+++ src/providers/store/services/api.js
@@ -1,11 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:3100/v1',
+  baseUrl: import.meta.env.VITE_BACKEND_URL,
 });
 
 export const api = createApi({
   baseQuery,
-  tagTypes: ['Users', 'Categories', 'Subcategories'],
   endpoints: () => ({}),
 });
